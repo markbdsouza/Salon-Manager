73fec346193e9762cf2f5c3c48437a3a1e5ca8cc
Mark B DSouza <mark.benjamin.dsouza@gmail.com>
2021-01-14 00:08:28 +0530

    basic backend

---
+++ Backend/src/main/java/com/markbdsouza/salonmanager/SalonManagerApplication.java
@@ -0,0 +1,13 @@
+package com.markbdsouza.salonmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonManagerApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/markbdsouza/salonmanager/SalonManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.markbdsouza.salonmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
